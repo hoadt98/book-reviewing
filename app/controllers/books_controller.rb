7941f801a41a034dcb2900ada7c5fab9c760ade8
Hoa Duong <tahado98@gmail.com>
2019-08-23 13:43:07 +0700

    Created

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  before_action :load_book, only: %i(show)
+
+  def show; end
+
+  def index
+    @books = Book.book_order
+  end
+
+  private
+
+    def load_book
+      @book = Book.find_by id: params[:id]
+      return if @book
+      flash[:none] = t "none"
+      redirect_to root_path
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,40 @@
+require "ffaker"
+
 User.create!(user_name: "Duong Thanh Hoa",
              email: "tahado98@gmail.com",
              password: "hoadt98",
              password_confirmation: "hoadt98",
              created_at: Time.zone.now)
+
+10.times do
+  name  = FFaker::Book.author
+  intro = FFaker::Book.description
+  Author.create!(author_name: name,
+                intro: intro)
+end
+
+4.times do
+  name  = FFaker::Book.genre
+  Category.create!(category_name: name)
+end
+
+4.times do
+  name  = FFaker::Company.name
+  Publishhouse.create!(publishhouse_name: name)
+end
+
+10.times do
+  name  = FFaker::Book.title
+  cover = "/assets/#{rand (1..2)}.jpg"
+  description = FFaker::Book.description
+  category_id = rand(1..4)
+  author_id = rand(1..10)
+  publishhouse_id = rand (1..4)
+  Book.create!(book_name: name,
+              description: description,
+              cover: cover,
+              category_id: category_id,
+              author_id: author_id,
+              publishhouse_id: publishhouse_id,
+              created_at: Time.zone.now)
+end
