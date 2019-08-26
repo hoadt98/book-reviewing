6c4bf770eaf952becd697c69dc8b652fed7bf334
Hoa Duong <tahado98@gmail.com>
2019-08-26 13:30:25 +0700

    Created
    Updated
    
    Updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :get_categories
+
   protect_from_forgery with: :exception
 
   include SessionsHelper
 
   before_action :set_locale
+
+  protected
+
+  def get_categories
+    @categories = Category.all
+  end
 
   private
 
